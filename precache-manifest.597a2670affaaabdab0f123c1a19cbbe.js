1d8d2d194276d3486feafcd6e87754a6d4010a84
bumblexq <bumblexq@gmail.com>
2022-04-04 18:24:21 +0300

    Updates

---
+++ precache-manifest.597a2670affaaabdab0f123c1a19cbbe.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44f5b78ed8b02756212a34ce5e4dbf07",
+    "url": "./index.html"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "2075808b747d6644b719",
+    "url": "./static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "700cd0117cd74ea95886",
+    "url": "./static/js/2.bcb1d5ed.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "./static/js/2.bcb1d5ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2075808b747d6644b719",
+    "url": "./static/js/main.cd9b7a6c.chunk.js"
+  },
+  {
+    "revision": "cf45432d129a816c05f6",
+    "url": "./static/js/runtime-main.dd0e4089.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "./static/media/persik.4e1ec840.png"
+  }
+]);
